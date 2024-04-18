8f566e13ae252aacd4919e7b6a8f0e6b8d92ebe8
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-04-18 22:11:35 +0530

    feat: hyprland config

--- home.nix
+++ home.nix
@@ -3,7 +3,16 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
+    ${pkgs.waybar}/bin/waybar &
+    ${pkgs.swww}/bin/swww init &
+
+    sleep 1
+
+    ${pkgs.swww}/bin/swww img ${./wallpaper.jpg} &
+  '';
+in {
   home.username = "ajmalmohad";
   home.homeDirectory = "/home/ajmalmohad";
 
@@ -14,7 +23,26 @@
   home.file = {
   };
 
-  home.sessionVariables = {
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    plugins = [
+      inputs.hyprland-plugins.packages."${pkgs.system}".borders-plus-plus
+    ];
+
+    settings = {
+      "plugin:borders-plus-plus" = {
+        add_borders = 1; # 0 - 9
+
+        # you can add up to 9 borders
+        "col.border_1" = "rgb(ffffff)";
+        "col.border_2" = "rgb(2222ff)";
+
+        border_size_1 = 10;
+        border_size_2 = -1;
+        natural_rounding = "yes";
+      };
+    };
   };
 
   programs.home-manager.enable = true;
