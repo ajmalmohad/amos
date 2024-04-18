a6b9738fa070bdf0ab6d40f1f04db4f79183510d
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-04-18 23:03:58 +0530

    feat: hyprland config

--- home.nix
+++ home.nix
@@ -12,37 +12,56 @@
 
     ${pkgs.swww}/bin/swww img ${./wallpaper.jpg} &
   '';
+  workspaces = builtins.concatLists (builtins.genList (
+      x: let
+        ws = let
+          c = (x + 1) / 10;
+        in
+          builtins.toString (x + 1 - (c * 10));
+      in [
+        "$mod, ${ws}, workspace, ${toString (x + 1)}"
+        "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+      ]
+    )
+    10);
 in {
   home.username = "ajmalmohad";
   home.homeDirectory = "/home/ajmalmohad";
   wayland.windowManager.hyprland.enable = true;
+
   wayland.windowManager.hyprland.settings = {
     "$mod" = "ALT";
+
+    bindm = [
+      "$mod, mouse:272, movewindow"
+      "$mod, mouse:273, resizewindow"
+      "$mod ALT, mouse:272, resizewindow"
+    ];
+
     bind =
       [
-        "$mod, F, exec, vivaldi"
-        ", Print, exec, grimblast copy area"
-      ]
-      ++ (
-        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (
-            x: let
-              ws = let
-                c = (x + 1) / 10;
-              in
-                builtins.toString (x + 1 - (c * 10));
-            in [
-              "$mod, ${ws}, workspace, ${toString (x + 1)}"
-              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            ]
-          )
-          10)
-      );
-  };
+        "$mod SHIFT, E, exec, pkill Hyprland"
+        "$mod, Q, killactive,"
+        "$mod, F, fullscreen,"
+        "$mod, G, togglegroup,"
+        "$mod SHIFT, N, changegroupactive, f"
+        "$mod SHIFT, P, changegroupactive, b"
+        "$mod, R, togglesplit,"
+        "$mod, T, togglefloating,"
+        "$mod, P, pseudo,"
+        "$mod ALT, ,resizeactive,"
 
-  home.packages = [
-    pkgs.htop
-  ];
+        # terminal
+        "$mod, Return, exec, alacritty"
+
+        # move focus
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+      ]
+      ++ workspaces;
+  };
 
   home.file = {
   };
