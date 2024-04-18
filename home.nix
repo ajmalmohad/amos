e33a9c6921875202370a4d88770361b126ab4b1a
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-04-18 22:17:20 +0530

    feat: hyprland settings

--- home.nix
+++ home.nix
@@ -16,6 +16,29 @@ in {
   home.username = "ajmalmohad";
   home.homeDirectory = "/home/ajmalmohad";
   wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland.settings = {
+    "$mod" = "ALT";
+    bind =
+      [
+        "$mod, F, exec, firefox"
+        ", Print, exec, grimblast copy area"
+      ]
+      ++ (
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
+  };
 
   home.packages = [
     pkgs.htop
