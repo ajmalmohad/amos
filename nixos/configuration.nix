c0f2ccca8d14f2950d906dd385fa96742b85fe7e
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-04-20 11:25:56 +0530

    feat: i3 wm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -112,22 +112,6 @@
     zsh.enable = true;
   };
 
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = ["graphical-session.target"];
-      wants = ["graphical-session.target"];
-      after = ["graphical-session.target"];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
-
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";
